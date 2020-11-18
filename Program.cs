41cbc61060dad0764cc316b6edffcd881bb813e3
Robert Sena <robert.sena25@gmail.com>
2020-11-18 15:37:28 -0300

    Projeto iniciado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LacosWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
