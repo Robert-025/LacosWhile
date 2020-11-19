eff5d9a4cc29a23562fecdc3ff83a691ffc6831f
Robert Sena <robert.sena25@gmail.com>
2020-11-18 23:15:26 -0300

    Tabuada até o 10 feita usando for

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace LacosWhile
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Qual o número que você deseja ver a tabuada? ");
+            int resposta = int.Parse(Console.ReadLine());
+
+            for(int contador = 0; contador <= 10; contador++){
+            
+            int resultado = resposta * contador;
+
+                Console.WriteLine($"{resposta} x {contador} = {resultado} ");
+            }
+
+            
         }
     }
 }
